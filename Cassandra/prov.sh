eac0e611b5e491a3d4e6fecc58addf0abcee3496
OXIDEN <oxiden.dd@gmail.com>
2015-01-18 00:43:08 +0900

    fix: Cassandra Cluster

--- Cassandra/prov.sh
+++ Cassandra/prov.sh
@@ -1,28 +1,46 @@
+# parameter:
+#  $1: IP Address of node
+
 #install rvm
 #\curl -sSL https://get.rvm.io | bash -s stable
 #source ~/.bash_profile
 
 # install misc.
-sudo ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
-sudo yum install -y ntp
-sudo /etc/init.d/ntpd start
-sudo /sbin/chkconfig ntpd on
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+yum install -y ntp
+/etc/init.d/ntpd start
+chkconfig ntpd on
 
 # install cassandra
-sudo rpm -ivh /vagrant/jre-7u67-linux-x64.rpm
-sudo tar xz -C /usr/local -f /vagrant/apache-cassandra-1.2.11-bin.tar.gz
-sudo ln -s /usr/local/apache-cassandra-1.2.11 /usr/local/cassandra
+rpm -iv /vagrant/jre-7u67-linux-x64.rpm
+tar xz -C /usr/local -f /vagrant/apache-cassandra-1.2.11-bin.tar.gz
+ln -s /usr/local/apache-cassandra-1.2.11 /usr/local/cassandra
+\rm -f /usr/local/cassandra/bin/*.bat
 # java -version
-sudo cp /vagrant/cassandra /etc/init.d/cassandra
-sudo chmod 755 /etc/init.d/cassandra
+cp /vagrant/cassandra /etc/init.d/cassandra
+chmod 755 /etc/init.d/cassandra
+sed -i -e "s/listen_address: localhost/listen_address: $1/" /usr/local/cassandra/conf/cassandra.yaml
+sed -i -e "s/rpc_address: localhost/rpc_address: 0.0.0.0/" /usr/local/cassandra/conf/cassandra.yaml
+sed -i -e 's/- seeds: "127.0.0.1"/- seeds: "192.168.33.10,192.168.33.11,192.168.33.12"/' /usr/local/cassandra/conf/cassandra.yaml
 # => The stack size specified is too small, Specify at least 228k
-sudo sed -i -e 's/JVM_OPTS -Xss[0-9]\+[km]/JVM_OPTS -Xss256k/g' /usr/local/cassandra/conf/cassandra-env.sh
-# => Error: Exception thrown by the agent : java.net.MalformedURLException: Local host name unknown: java.net.UnknownHostException: vagrant-centos65.vagrantup.com: vagrant-centos65.vagrantup.com# sed -e /etc/sysconfig/local
-sudo sh -c 'echo "127.0.0.1   $(hostname)" >> /etc/hosts'
+sed -i -e 's/JVM_OPTS -Xss[0-9]\+[km]/JVM_OPTS -Xss256k/g' /usr/local/cassandra/conf/cassandra-env.sh
+# => Error: Exception thrown by the agent : java.net.MalformedURLException: Local host name unknown: java.net.UnknownHostException: vagrant-centos65.vagrantup.com: vagrant-centos65.vagrantup.com
+# JNA
+#cp /vagrant/{jna-4.1.0.jar,jna-platform-4.1.0.jar} /usr/local/cassandra/lib/
+#echo 'root             soft    memlock         unlimited' >> /etc/security/limits.conf
+#echo 'root             hard    memlock         unlimited' >> /etc/security/limits.conf
+echo "192.168.33.10   db1.localdomain" >> /etc/hosts
+echo "192.168.33.11   db2.localdomain" >> /etc/hosts
+echo "192.168.33.12   db3.localdomain" >> /etc/hosts
+# set fd-limit, etc... #sysctl -w xxxx=yyyyy
+
+# PATH
+echo 'export PATH=/usr/local/cassandra/bin:$PATH' >> ~/.bash_profile
+echo 'export PATH=/usr/local/cassandra/bin:$PATH' >> ~vagrant/.bash_profile
 
 # start cassandra
-sudo chkconfig --add cassandra
-#sudo service cassandra start
+chkconfig --add cassandra
+#service cassandra start
 
 # done
-touch /tmp/provisioned.done
+date > /tmp/provisioned.done
